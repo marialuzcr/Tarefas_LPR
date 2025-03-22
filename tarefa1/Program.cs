50175c87eaba6bed506f505568849e9c351bc280
marialuzcr <seuemailgithub@email.com.br>
2025-03-22 14:49:58 -0300

    aula4

---
+++ tarefa1/Program.cs
@@ -0,0 +1,22 @@
+﻿int x;
+Console.ForegroundColor = ConsoleColor.Yellow ;
+Console.WriteLine("--------------");
+Console.WriteLine("   tarefa 1   ");
+Console.WriteLine("--------------");
+Console.WriteLine("digite um numero");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.ResetColor();
+
+x= Convert.ToInt32(Console.ReadLine());
+
+if(x%2==0)
+{
+    Console.ForegroundColor = ConsoleColor.Green; 
+    Console.WriteLine("seu numero é par ");
+}
+else{
+     Console.ForegroundColor = ConsoleColor.DarkRed; 
+     Console.WriteLine("seu numero é impar ");
+}
+ Console.ResetColor(); 
---
+++ tarefa3/Program.cs
@@ -0,0 +1,37 @@
+﻿
+string classe;
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("---------------");
+Console.WriteLine("|  tarefa3    |");
+Console.WriteLine("---------------");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("escolha seu personagem entre:\n guerreira\n  mago\n arqueira\n");
+Console.ResetColor();
+
+classe =  Console.ReadLine();;
+switch (classe) 
+{
+ case "guerreira":
+ Console.ForegroundColor = ConsoleColor.DarkGreen;
+ Console.WriteLine("seus poderes são: ataque pesado, defesa total");
+ break;
+ 
+ case "mago":
+ Console.ForegroundColor = ConsoleColor.DarkMagenta;
+ Console.WriteLine("seus poderes são: Bola de Fogo, Escudo de Gelo ");
+ break;
+
+ case "arqueira":
+ Console.ForegroundColor = ConsoleColor.Gray;
+ Console.WriteLine("seus poderes são: Flecha Precisa, Disparo Triplo");
+ break;
+
+    default:
+    Console.WriteLine("nada foi indentificao");
+ break;
+}
