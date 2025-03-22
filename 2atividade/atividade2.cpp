eac63622e9a7108ef6ff75217401867e8ea23830
marialuzcr <seuemailgithub@email.com.br>
2025-03-22 17:44:09 -0300

    aula4

---
+++ 2atividade/atividade2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main(){
+    using namespace std;
+
+int a;
+int b;
+cout<<"digite um numero\n";
+cin>>a;
+cout<<"digite um numero\n";
+cin>>b;
+if(a%b==0|| b%a==0){
+    cout<<"seu numero eh multiplo\n";
+}
+else {
+    cout<<"seu numero nao eh multiplo\n";
+}
